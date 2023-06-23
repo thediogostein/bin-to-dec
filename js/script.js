4e16f7b4b4136c7df83dba1f25145c56fbdde67c
bruxo <diogo.stein@outlook.com.br>
2023-06-23 12:26:37 -0300

    Refactor validation

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
 const form = document.querySelector('form');
+const convertBtn = document.querySelector('button[type="submit"');
+const resetBtn = form.querySelector('button[type="reset"]');
+const errorEl = document.getElementById('error-el');
+
+const showError = () => {
+  errorEl.textContent = 'Not a binary, reset and try again';
+  convertBtn.disabled = true;
+  form.querySelector('#binary').disabled = true;
+  form.querySelector('#result-el').disabled = true;
+};
+
+const resetForm = () => {
+  errorEl.textContent = '';
+  convertBtn.disabled = false;
+  form.querySelector('#binary').disabled = false;
+  form.querySelector('#result-el').disabled = false;
+};
 
 const convert = (e) => {
   e.preventDefault();
 
   const binary = form.querySelector('#binary').value;
-  const resultEl = form.querySelector('#result-el');
-  let result;
+  let resultEl = form.querySelector('#result-el');
 
-  // Clear result
-  resultEl.value = '';
+  // Clear result before showing next result
+  // resultEl.textContent = '';
 
   for (let i = 0; i < binary.length; i++) {
     if (binary[i] === '0' || binary[i] === '1') {
-      result = 'Binary';
+      resultEl.value = binary;
+      //
+
+      //
     } else {
-      result = 'Not a binary';
+      showError();
     }
   }
-
-  console.log(result);
-
-  // Outputs result
-  result.value = binary;
 };
 
 form.addEventListener('submit', convert);
+resetBtn.addEventListener('click', resetForm);
